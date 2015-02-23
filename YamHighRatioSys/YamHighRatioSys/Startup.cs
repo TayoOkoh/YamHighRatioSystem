b5afaa5a085c5a2fc6d0fa914213f300f2966efa
kenorem <emyaku@yahoo.com>
2015-02-23 14:20:32 +0100

    Initial code generation in Visual Studio 2013
    Code generation

---
+++ YamHighRatioSys/YamHighRatioSys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YamHighRatioSys.Startup))]
+namespace YamHighRatioSys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
